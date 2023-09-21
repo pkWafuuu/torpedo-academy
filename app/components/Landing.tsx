5f8902e157bd3c54f162696ecf61016eb82ef57c
Phil <69749448+pkWafuuu@users.noreply.github.com>
2023-09-21 23:18:01 +0400

    commit

---
+++ app/components/Landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <section className='bg-[#0d3e84] h-[calc(100vh-80px)]'>
+
+    </section>
+  )
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+import logo from "../../public/icons/logo-horizontal-white.png";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="h-28 flex-center bg-[#0d3e84] text-white">
+      <div className="row flex justify-between">
+        <Image src={logo} objectFit="cover" alt="" className="w-[170px]" />
+        <ul className="hidden md:flex md:items-center">
+          <li className="nav__link">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="nav__link">
+            <Link href="/programs">Programs</Link>
+          </li>
+          <li className="nav__link">
+            <Link href="/locations">Locations</Link>
+          </li>
+          <li className="nav__link">
+            <Link href="/about">About Us</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
