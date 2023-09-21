d81eb68b6518cc090ab3107de6a5f6665b953c42
Phil <69749448+pkWafuuu@users.noreply.github.com>
2023-09-21 23:36:32 +0400

    test

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Landing() {
   return (
-    <section className='bg-[#0d3e84] h-[calc(100vh-80px)]'>
-
+    <section className='bg-[#0d3e84] h-[calc(100vh-112px)] text-center'>
+        IZZA PRANK
     </section>
   )
 }
