7ffb93a049f77c909a7a736c7ee7b757b9c44617
Phil <69749448+pkWafuuu@users.noreply.github.com>
2023-09-25 13:30:26 +0400

    added carousel

--- app/components/HomePrograms.tsx
+++ app/components/HomePrograms.tsx
@@ -1,17 +1,61 @@
-import React from "react";
+"use client"
+import React, { Component } from "react";
 import Program from "../ui/Program";
-import privateclass from "../../public/gallery/qwer.png";
+import groupclass from "../../public/gallery/qwer.png";
+import privateclass from "../../public/gallery/privateclass.png"
+import babyclass from "../../public/gallery/babyclass.png"
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
 
-export default function HomePrograms() {
+export default class HomePrograms extends Component {
+    render(){
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      };
+
   return (
     <div className="row py-10 text-center">
       <h1>Programs</h1>
-      <div className="flex flex-wrap">
+      <Slider {...settings}>
+      {/* <div className="flex flex-wrap"> */}
         <Program pic={privateclass} title="Private Class" text="lorem ipsum gypsum burger mac and burger meal plus spaghetti meatballs" />
-        <Program pic={privateclass} title="Group Class" text="description" />
-        <Program pic={privateclass} title="Baby Class" text="description" />
-        <Program pic={privateclass} title="Private Class" text="description" />
-      </div>
+        <Program pic={groupclass} title="Group Class" text="description" />
+        <Program pic={babyclass} title="Baby Class" text="description" />
+        <Program pic={groupclass} title="GX Class" text="description" />
+      {/* </div> */}
+      </Slider>
     </div>
   );
+    }
 }
--- app/ui/Program.tsx
+++ app/ui/Program.tsx
@@ -1,19 +1,22 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-export default function Program({pic, title, text}:any) {
+export default function Program({ pic, title, text }: any) {
   return (
-    <div className='rounded p-2 md:w-1/2 lg:w-1/3'>
-        <div className='rounded h-full shadow-xl'>
-            <Image 
-                src={pic}
-                objectFit="cover"
-                alt="Image"
-                className='mb-4 rounded-t'
-            />
+    <div className="rounded p-2">
+      <div className="rounded h-full shadow-xl pb-4 relative">
+        <Image
+          src={pic}
+          objectFit="cover"
+          alt="Image"
+          className="mb-4 rounded-t"
+        />
+        <h1>{title}</h1>
+        <div className="overlay flex-center flex-col">
             <h1>{title}</h1>
-            <p className='mb-4'>{text}</p>
+            <p className="mb-4">{text}</p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
